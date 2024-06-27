369758e9861f70d8888129938ce476f5fa456f15
MocicaRazvan <razvanmocica@gmail.com>
2024-06-27 17:41:58 +0300

    next docker

--- client-micro/src/app/session-wrapper.tsx
+++ client-micro/src/app/session-wrapper.tsx
@@ -1,39 +1,41 @@
 "use client";
 
-import { ReactNode, useEffect, useState } from "react";
-import { useSession } from "next-auth/react";
-import { StompProvider } from "@/providers/stomp-provider";
-import { ChatProvider } from "@/context/chat-context";
-import { ChatMessageNotificationProvider } from "@/context/chat-message-notification-context";
+import {ReactNode, useEffect, useState} from "react";
+import {useSession} from "next-auth/react";
+import {StompProvider} from "@/providers/stomp-provider";
+import {ChatProvider} from "@/context/chat-context";
+import {ChatMessageNotificationProvider} from "@/context/chat-message-notification-context";
 import LoadingSpinner from "@/components/common/loading-spinner";
-import { Session } from "next-auth";
+import {Session} from "next-auth";
 
-export default function SessionWrapper({ children }: { children: ReactNode }) {
-  const spring = process.env.NEXT_PUBLIC_SPRING_CLIENT!;
-  const { data: session, status } = useSession();
-  const [authUser, setAuthUser] = useState<Session["user"]>(undefined);
+export default function SessionWrapper({children}: { children: ReactNode }) {
+    const spring = process.env.NEXT_PUBLIC_SPRING_CLIENT!;
+    const {data: session, status} = useSession();
+    const [authUser, setAuthUser] = useState<Session["user"]>(undefined);
 
-  useEffect(() => {
-    if (status === "authenticated" && session?.user) {
-      setAuthUser(session.user);
-    } else {
-      setAuthUser(undefined);
-    }
-  }, [status, session]);
+    console.log("spring stomp", spring)
 
-  if (status === "loading") return <LoadingSpinner />;
+    useEffect(() => {
+        if (status === "authenticated" && session?.user) {
+            setAuthUser(session.user);
+        } else {
+            setAuthUser(undefined);
+        }
+    }, [status, session]);
 
-  // return !authUser ? (
-  //   <>{children}</>
-  // ) : (
-  return (
-    <StompProvider url={spring + "/ws/ws-service"} authUser={authUser}>
-      <ChatProvider authUser={authUser}>
-        <ChatMessageNotificationProvider authUser={authUser}>
-          {children}
-        </ChatMessageNotificationProvider>
-      </ChatProvider>
-    </StompProvider>
-  );
-  // );
+    if (status === "loading") return <LoadingSpinner/>;
+
+    // return !authUser ? (
+    //   <>{children}</>
+    // ) : (
+    return (
+        <StompProvider url={spring + "/ws/ws-service"} authUser={authUser}>
+            <ChatProvider authUser={authUser}>
+                <ChatMessageNotificationProvider authUser={authUser}>
+                    {children}
+                </ChatMessageNotificationProvider>
+            </ChatProvider>
+        </StompProvider>
+    );
+    // );
 }
